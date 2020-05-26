7e75cb1ab4f80da2758e0a0d728c1ae9dd02e7a8
Carlos Morales <carlos_cc_13@hotmail.com>
2020-05-26 23:03:41 +0200

    changes procfile

--- Setup.sh
+++ Setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
+echo "\
 [general]\n\
-email = \”carlos_cc_13@hotmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
